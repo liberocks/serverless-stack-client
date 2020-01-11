d135c006094868c4ff84b8357e57e36c1f24261c
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-01-11 21:28:40 +0700

    [UPDATE] Sign-up, confirmation, and sign-in flow

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Home from './containers/Home'
-import NotFound from './containers/NotFound'
+import AppliedRoute from './components/AppliedRoute'
 
-export default function Routes () {
+import Home from './containers/Home'
+import Login from './containers/Login'
+import NotFound from './containers/NotFound'
+import Signup from './containers/Signup'
+
+export default function Routes ({ appProps }) {
   return (
     <Switch>
-      <Route path='/' exact component={Home} />
+      <AppliedRoute appProps={appProps} path='/' exact component={Home} />
+      <AppliedRoute appProps={appProps} path='/login' exact component={Login} />
+      <AppliedRoute appProps={appProps} path='/signup' exact component={Signup} />
+      {/* Finally, catch all unmatched routes */}
       <Route component={NotFound} />
     </Switch>
   )
