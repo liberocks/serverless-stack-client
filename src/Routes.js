1ef5238baa10d74e4785cdcc0ac6ede8bedec62f
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-01-12 20:58:33 +0700

    [UPDATE] Create note page

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import Home from './containers/Home'
 import Login from './containers/Login'
 import NotFound from './containers/NotFound'
 import Signup from './containers/Signup'
+import NewNote from './containers/NewNote'
 
 export default function Routes ({ appProps }) {
   return (
@@ -14,6 +15,7 @@ export default function Routes ({ appProps }) {
       <AppliedRoute appProps={appProps} path='/' exact component={Home} />
       <AppliedRoute appProps={appProps} path='/login' exact component={Login} />
       <AppliedRoute appProps={appProps} path='/signup' exact component={Signup} />
+      <AppliedRoute appProps={appProps} path='/notes/new' exact component={NewNote} />
       {/* Finally, catch all unmatched routes */}
       <Route component={NotFound} />
     </Switch>
